901cd0ae8b80ba36054623447297469da70056ff
seanwooj <seanwoojinkim@gmail.com>
2015-05-20 11:53:35 -0700

    show and hide new vice link and add a param method in the controller

--- app/assets/javascripts/controllers/add_user_vice_controller.js
+++ app/assets/javascripts/controllers/add_user_vice_controller.js
@@ -2,7 +2,13 @@ window.app.controller('AddUserVicesController', [
   '$scope', 'vices',
   function ($scope, vices) {
     $scope.vices = vices.vices;
-    
 
+    $scope.makeParam = function () {
+      if ( $scope.viceSearch ) {
+        $scope.viceNameParam = jQuery.param({vice_name: $scope.viceSearch});
+      } else {
+        $scope.viceNameParam = '';
+      }
+    };
   }
 ]);
