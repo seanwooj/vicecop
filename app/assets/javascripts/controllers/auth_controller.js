b86368bbad45b9bf8c72fe63de0c540d9ad9223f
seanwooj <seanwoojinkim@gmail.com>
2015-05-21 21:12:34 -0700

    added shakey thing when you get your login wrong

--- app/assets/javascripts/controllers/auth_controller.js
+++ app/assets/javascripts/controllers/auth_controller.js
@@ -4,7 +4,7 @@ window.app.controller('AuthController', [
     $scope.login = function () {
       Auth.login($scope.user).then(function () {
       $state.go('home');
-      });
+      }, shakeIt );
     }
 
     $scope.register = function () {
@@ -12,5 +12,16 @@ window.app.controller('AuthController', [
         $state.go('home');
       });
     };
+
+    var shakeIt = function () {
+      // NOT GOOD. SHOULN'T DO
+      // turn this into a directive ASAP.
+      // I don't know how to do this yet.
+      // CHANGE THIS CHANGE THIS
+      $('.form-group').addClass('shake shake-horizontal');
+      setTimeout(function(){
+        $('.form-group').removeClass('shake shake-horizontal');
+      }, 500);
+    }
   }
 ]);
