e4f5f8ca6cdd0944c263edf260c38b6bda6a866a
seanwooj <seanwoojinkim@gmail.com>
2015-05-30 15:46:44 -0700

    Add back button to more screens

--- app/assets/javascripts/controllers/new_vices_controller.js
+++ app/assets/javascripts/controllers/new_vices_controller.js
@@ -3,6 +3,10 @@ window.app.controller('NewVicesController', [
   function ($scope, vices, $location, $state) {
     $scope.viceName = $location.search()['vice_name'];
 
+    $scope.goBack = function () {
+      window.history.back();
+    };
+
     $scope.addVice = function(){
       vices.addVice({name: $scope.viceName}).then( function (res) {
         $state.go('user_vice', {id: res.data.id });
